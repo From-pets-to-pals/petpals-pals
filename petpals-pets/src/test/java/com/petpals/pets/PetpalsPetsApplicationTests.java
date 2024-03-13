e820d7a5671df9d02dd62d1517454d54ef957dcc
Sid B <sid.bennaceur@haidi.io>
2024-03-13 13:48:40 +0100

    init

---
+++ petpals-pets/src/test/java/com/petpals/pets/PetpalsPetsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.petpals.pets;
+
+import com.petpals.pets.bootstrap.PetpalsPetsApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {PetpalsPetsApplication.class})
+class PetpalsPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
