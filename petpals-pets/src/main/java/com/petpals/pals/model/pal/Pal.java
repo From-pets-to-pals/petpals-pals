c910427345eff976c4d23baab15413934dba3095
Sid B <sid.bennaceur@haidi.io>
2024-03-15 11:38:50 +0100

    validation rules

--- petpals-pets/src/main/java/com/petpals/pals/model/pal/Pal.java
+++ petpals-pets/src/main/java/com/petpals/pals/model/pal/Pal.java
@@ -26,28 +26,53 @@ public class Pal {
     @SpecieConstraint(enumClass = Species.class, message = "Invalid specie selected")
     private String specie;
     @Getter
+    @Size(min=3, max=25)
     private String race;
-    @Getter @Setter
+    @Getter
+    @Setter
     private Long owner;
-    @Getter private String icadIdentifier;
+    @Getter
+    @Size(min=15, max=15)
+    private String icadIdentifier;
     @NotNull
-    @Getter @Setter private boolean isVaccinated;
+    @Getter
+    @Setter
+    private boolean isVaccinated;
     @NotNull
-    @Getter private boolean isMale;
+    @Getter
+    private boolean isMale;
     @DecimalMin(value = "0.0", inclusive = false)
-    @Getter @Setter private Double weight;
+    @Getter
+    @Setter
+    private Double weight;
     @Min(value = 0)
-    @Getter @Setter private Double height;
-    @Pattern(regexp = "^(250(26|22))[0-9]{10}$")
-    @Getter @Setter private String id;
-    @Getter @Setter private List<String> medicalHistory;
+    @Getter
+    @Setter
+    private Double height;
+    @Pattern(regexp = "^[(250(26|22))[0-9]{10}]{15}$")
+    @Getter
+    @Setter
+    private String id;
+    @Getter
+    @Setter
+    private List<String> medicalHistory;
     @Future
-    @Getter @Setter private Date nextVaccine;
+    @Getter
+    @Setter
+    private Date nextVaccine;
     @Future
-    @Getter @Setter private Date nextPlannedVetApp;
-    @Getter @Setter private boolean isSterilized;
-    @Getter @Setter private boolean hasPassport;
-    @Getter @Setter private boolean hasDied;
+    @Getter
+    @Setter
+    private Date nextPlannedVetApp;
+    @Getter
+    @Setter
+    private boolean isSterilized;
+    @Getter
+    @Setter
+    private boolean hasPassport;
+    @Getter
+    @Setter
+    private boolean hasDied;
 
 
 }
