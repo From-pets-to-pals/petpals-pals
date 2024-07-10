0b698b163d94fb35ec9ec920ec12421c589b6887
Sid-Athos <sa.bennaceur@gmail.com>
2024-07-11 00:52:48 +0200

    find owner with pals by reference

--- petpals-pals/src/main/java/com/petpals/persistence/repositories/OwnersRepository.java
+++ petpals-pals/src/main/java/com/petpals/persistence/repositories/OwnersRepository.java
@@ -4,6 +4,9 @@ import com.petpals.persistence.entities.Owners;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @ApplicationScoped
 public class OwnersRepository implements PanacheRepository<Owners> {
@@ -11,5 +14,16 @@ public class OwnersRepository implements PanacheRepository<Owners> {
     public Long save(Owners owners){
         persistAndFlush(owners);
         return owners.getId();
+    }
+    
+    public Boolean doUserExist(String name, String password){
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", name);
+        params.put("password", password);
+        return find("name = :name and password = :password", params).firstResultOptional().isPresent();
+    }
+    
+    public Owners findByReference(String reference){
+        return find("reference = :reference", reference).firstResult();
     }
 }
